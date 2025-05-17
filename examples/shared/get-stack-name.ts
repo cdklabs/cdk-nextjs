b83648155bf12bba45689bcb19677ca1299d9bfd
Ben Stickley <bestickley@gmail.com>
2025-05-17 06:18:02 -0400

    fix: cfn stacks must start with character

--- examples/shared/get-stack-name.ts
+++ examples/shared/get-stack-name.ts
@@ -2,6 +2,10 @@
  * Important in e2e test since we set ENV to PR number
  */
 export function getStackName(stackName: string) {
-  const env = process.env["ENV"] ?? "dev";
+  let env = process.env["ENV"] ?? "dev";
+  if (!isNaN(Number(env))) {
+    // cloudformation stacks must begin with string
+    env = `pr-${env}`;
+  }
   return `${env}-${stackName}`;
 }
