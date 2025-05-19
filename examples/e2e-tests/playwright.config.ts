24af0995b7639bc1fd3fef69d00a927874dd7624
Ben Stickley <bestickley@gmail.com>
2025-05-19 09:32:03 -0400

    fix: cdk-nextjs=1 cookie for regional containers

--- examples/e2e-tests/playwright.config.ts
+++ examples/e2e-tests/playwright.config.ts
@@ -19,6 +19,12 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    /* Set cookies for all requests */
+    extraHTTPHeaders: {
+      // only required for rgnl-containers example to comply with security
+      Cookie: "cdk-nextjs=1",
+    },
   },
 
   /* Configure projects for major browsers */
